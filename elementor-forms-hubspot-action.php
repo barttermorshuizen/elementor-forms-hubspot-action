1bff908433bee32624cd37c25a03e8837be21819
barttermorshuizen <bart@moreawesome.co>
2025-05-23 11:05:18 +0200

    version number bump and update elementor tested versions

--- elementor-forms-hubspot-action.php
+++ elementor-forms-hubspot-action.php
@@ -4,14 +4,14 @@
  * Plugin Name: Elementor Forms Hubspot Action
  * Description: Custom addon which sends the form to Hubspot after form submission.
  * Plugin URI:  https://moreaweasome.co
- * Version:     0.11
+ * Version:     0.13
  * Author:      More Awesome B.V.
  * Author URI:  https://moreawesome.co
  * Text Domain: elementor-forms-hubspot-action
  *
  * Requires Plugins: elementor
- * Elementor tested up to: 3.20.0
- * Elementor Pro tested up to: 3.20.0
+ * Elementor tested up to: 3.29.0
+ * Elementor Pro tested up to: 3.29.0
  */
 
 require_once plugin_dir_path( __FILE__ ) . 'Hubspot_Action_Settings.php';
